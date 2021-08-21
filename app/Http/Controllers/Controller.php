8084a79f324c9466f3bbbf0d0685a608c461c6aa
DESKTOP-KCT3B76\wj <969576137@qq.com>
2021-08-21 16:39:39 +0800

    初始化GatewayWorker

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use GatewayClient\Gateway;
+use Illuminate\Auth\Access\Gate;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +12,9 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {
+        Gateway::$registerAddress = '127.0.0.1:1314';
+    }
 }
